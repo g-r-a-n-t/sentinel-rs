c12c7381cdd86af88430cd1825c53527489c03d9
Grant Wuerker <gwuerker@gmail.com>
2020-05-06 17:43:56 +0800

    .

--- src/lib.rs
+++ src/lib.rs
@@ -60,7 +60,7 @@ pub fn inject_metering(code: &[u8]) -> Result<Vec<u8>, parity_wasm::elements::Er
     };
 
     // making the gas import compatible with scout. remove this
-    let module_name = result.import_section().unwrap().entries()[result.import_section().unwrap().entries().len() - 2].module();
+    let module_name = result.import_section().unwrap().entries()[result.import_section().unwrap().entries().len() - 2].field();
     panic!("{}", module_name);
 
     parity_wasm::serialize(result)
